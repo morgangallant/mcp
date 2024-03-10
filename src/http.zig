9520bf6331a5c18e600b8d44ad1ba51b5e429933
Morgan Gallant <morgan@morgangallant.com>
2024-03-10 14:39:20 -0700

    cleaner http.Request type
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- src/http.zig
+++ src/http.zig
@@ -461,243 +461,54 @@ pub fn ServerType(
 }
 
 pub const Request = struct {
-    method: std.http.Method,
-    target: []const u8,
-    version: std.http.Version,
-    content_length: ?u64,
-    headers: std.http.Headers,
-    transfer_encoding: std.http.TransferEncoding,
-    transfer_compression: std.http.ContentEncoding,
+    head: std.http.Request.Head,
+    stage: enum { head, body, ready },
+    read_buffer: std.ArrayList(u8),
+    headers_end: usize,
 
-    stage: enum { start, headers, body, ready },
-    body: std.ArrayList(u8),
-
-    fn init(self: *Request, gpa: std.mem.Allocator) void {
-        self.* = .{
-            .method = undefined,
-            .target = undefined,
-            .version = undefined,
-            .content_length = null,
-            .transfer_encoding = .none,
-            .transfer_compression = .identity,
-            .headers = .{
-                .allocator = gpa,
-                .owned = true,
-            },
-            .stage = .start,
-            .body = std.ArrayList(u8).init(gpa),
-        };
-    }
-
-    fn reset(self: *Request) void {
-        self.method = undefined;
-        self.target = undefined;
-        self.version = undefined;
-        self.content_length = null;
-        self.headers.clearAndFree();
-        self.transfer_encoding = .none;
-        self.transfer_compression = .identity;
-        self.stage = .start;
-        self.body.clearAndFree();
-    }
-
-    fn consumeStream(self: *Request, incoming: []const u8) !usize {
-        std.debug.assert(self.stage != .ready);
+    pub fn consume(self: *Request, stream: []const u8) !usize {
         var consumed: usize = 0;
-        outer: while (incoming.len - consumed > 0) {
-            if (self.stage == .body) {
-                const remaining = if (self.content_length) |cl|
-                    cl - self.body.items.len
-                else
-                    0;
-                const take = @min(remaining, incoming.len);
-                if (take > 0) {
-                    try self.body.appendSlice(incoming[0..take]);
-                    consumed += take;
-                }
-                if (remaining - take == 0) self.stage = .ready;
-                break;
-            }
-            var buffer = incoming;
-            var index = std.mem.indexOf(u8, buffer, "\r\n");
-            while (index) |end| {
-                consumed += 2;
-                const entry = buffer[0..end];
-                if (entry.len == 0) {
-                    if (self.stage != .headers) return error.InvalidRequest;
-                    if (self.content_length) |_| {
-                        self.stage = .body;
-                        continue :outer;
+        outer: while (consumed < stream.len) {
+            switch (self.stage) {
+                .ready => break :outer,
+                .body => {
+                    const remaining = if (self.content_length) |cl|
+                        cl - self.body().len
+                    else
+                        0;
+                    const take = @min(remaining, stream.len - consumed);
+                    if (take > 0) {
+                        try self.read_buffer.appendSlice(stream[consumed .. consumed + take]);
+                        consumed += take;
                     }
-                    self.stage = .ready;
-                    break :outer;
-                }
-                consumed += entry.len;
-                try self.parseLine(entry);
-                buffer = buffer[end + 2 ..];
-                index = std.mem.indexOf(u8, buffer, "\r\n");
+                    if (remaining - take == 0) self.stage = .ready;
+                },
+                .head => {
+                    const remaining = stream[consumed..];
+                    const take = if (std.mem.indexOf(u8, remaining, "\r\n\r\n")) |stop| blk: {
+                        self.stage = .body;
+                        break :blk stop + 4;
+                    } else stream.len;
+                    try self.read_buffer.appendSlice(remaining[0..take]);
+                    if (self.stage == .body) {
+                        self.head = try std.http.Request.Head.parse(&self.read_buffer.items);
+                        self.headers_end = self.read_buffer.items.len;
+                    }
+                    consumed += take;
+                },
             }
-            break;
         }
         return consumed;
     }
 
-    fn parseLine(self: *Request, line: []const u8) !void {
-        std.debug.assert(line.len > 0);
-        switch (self.stage) {
-            .start => {
-                if (line.len < 10) return error.InvalidRequest;
-
-                const method_end = std.mem.indexOfScalar(u8, line, ' ');
-                if (method_end == null or method_end.? > 24) return error.InvalidRequest;
-
-                const version_start = std.mem.lastIndexOfScalar(u8, line, ' ');
-                if (version_start == null or version_start == method_end.?)
-                    return error.InvalidRequest;
-                const version_str = line[version_start.? + 1 ..];
-
-                self.method = @as(
-                    std.http.Method,
-                    @enumFromInt(std.http.Method.parse(line[0..method_end.?])),
-                );
-                self.target = line[method_end.? + 1 .. version_start.?];
-                self.version = if (stdx.eqlComptime(u8, version_str, "HTTP/1.0"))
-                    .@"HTTP/1.0"
-                else if (stdx.eqlComptime(u8, version_str, "HTTP/1.1"))
-                    .@"HTTP/1.1"
-                else
-                    return error.InvalidRequest;
-
-                self.stage = .headers;
-            },
-            .headers => {
-                switch (line[0]) {
-                    ' ', '\t' => return error.HeaderContinuationsUnsupported,
-                    else => {},
-                }
-
-                var iter = std.mem.tokenizeAny(u8, line, ": ");
-                const name = iter.next() orelse return error.InvalidHeader;
-                const value = iter.rest();
-
-                try self.appendHeader(name, value);
-            },
-            else => unreachable,
-        }
+    pub fn iterateHeaders(self: *Request) std.http.HeaderIterator {
+        return std.http.HeaderIterator.init(self.read_buffer[0..self.headers_end]);
     }
 
-    fn appendHeader(self: *Request, name: []const u8, value: []const u8) !void {
-        try self.headers.append(name, value);
-
-        if (std.ascii.eqlIgnoreCase(name, "content-length")) {
-            if (self.content_length != null) return error.InvalidHeader;
-            self.content_length = std.fmt.parseInt(u8, value, 10) catch {
-                return error.InvalidContentLength;
-            };
-        } else if (std.ascii.eqlIgnoreCase(name, "transfer-encoding")) {
-            var components = std.mem.splitBackwardsScalar(u8, value, ',');
-
-            const first = components.first();
-            const trimmed_first = std.mem.trim(u8, first, " ");
-
-            var next: ?[]const u8 = first;
-            if (std.meta.stringToEnum(std.http.TransferEncoding, trimmed_first)) |transfer| {
-                if (self.transfer_encoding != .none) return error.InvalidHeader;
-                self.transfer_encoding = transfer;
-                next = components.next();
-            }
-
-            if (next) |second| {
-                const trimmed_second = std.mem.trim(u8, second, " ");
-                if (std.meta.stringToEnum(std.http.ContentEncoding, trimmed_second)) |content| {
-                    if (self.transfer_compression != .identity) return error.InvalidHeader;
-                    self.transfer_compression = content;
-                } else {
-                    return error.UnsupportedTransferEncoding;
-                }
-            }
-
-            if (components.next()) |_| return error.UnsupportedTransferEncoding;
-        } else if (std.ascii.eqlIgnoreCase(name, "content-encoding")) {
-            if (self.transfer_compression != .identity) return error.InvalidHeader;
-            const trimmed = std.mem.trim(u8, value, " ");
-            if (std.meta.stringToEnum(std.http.ContentEncoding, trimmed)) |content| {
-                self.transfer_compression = content;
-            } else {
-                return error.UnsupportedTransferEncoding;
-            }
-        }
+    pub fn body(self: *const Request) []const u8 {
+        std.debug.assert(self.stage != .head);
+        return self.read_buffer[self.headers_end..];
     }
 };
 
-pub const ResponseWriter = struct {
-    status: std.http.Status,
-    headers: std.http.Headers,
-    body: std.ArrayList(u8),
-    preamble: std.ArrayList(u8),
-    sent: usize,
-
-    fn init(self: *ResponseWriter, gpa: std.mem.Allocator) void {
-        self.* = .{
-            .status = .ok,
-            .headers = .{
-                .allocator = gpa,
-                .owned = true,
-            },
-            .body = std.ArrayList(u8).init(gpa),
-            .preamble = std.ArrayList(u8).init(gpa),
-            .sent = 0,
-        };
-    }
-
-    fn deinit(self: *ResponseWriter) void {
-        self.headers.clearAndFree();
-        self.body.deinit();
-        self.preamble.deinit();
-        self.* = undefined;
-    }
-
-    fn reset(self: *ResponseWriter) void {
-        self.status = .ok;
-        self.headers.clearAndFree();
-        self.body.clearAndFree();
-        self.preamble.clearAndFree();
-        self.sent = 0;
-    }
-};
-
-fn ConcurrentMemoryPoolType(comptime T: type) type {
-    return struct {
-        const Self = @This();
-        const InnerType = std.heap.MemoryPool(T);
-
-        inner: InnerType,
-        lock: std.Thread.Mutex,
-
-        fn init(allocator: std.mem.Allocator) Self {
-            return .{
-                .inner = InnerType.init(allocator),
-                .lock = .{},
-            };
-        }
-
-        fn deinit(self: *Self) void {
-            self.inner.deinit();
-            self.* = undefined;
-        }
-
-        const ItemPtr = *align(InnerType.item_alignment) T;
-
-        fn create(self: *Self) !ItemPtr {
-            self.lock.lock();
-            defer self.lock.unlock();
-            return try self.inner.create();
-        }
-
-        fn destroy(self: *Self, ptr: ItemPtr) void {
-            self.lock.lock();
-            defer self.lock.unlock();
-            return self.inner.destroy(ptr);
-        }
-    };
-}
+const ResponseWriter = struct {};
